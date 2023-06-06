541e13924c8e565a14046286090f7db32eb8e0d1
unknown <vladimir_ostrovski@web.de>
2023-06-06 17:56:44 +0200

    work on distance

---
+++ curveFittingMEP.R
@@ -0,0 +1,85 @@
+#source("asymptoticTestBootstrapVariance.R")
+#source("BootstrapTestTPercentile.R")
+library(minpack.lm)
+
+none=""
+asymptoticBV="asymptoticBootstrapVariance"
+tPercentileBootstrap="tPercentileBootstrap"
+
+curveFittingMEP<-function(formula,data, test, alpha=0.05,
+                        nSimulation=200){
+  
+  #initial information
+  ls=list()
+  ls$data=data
+  ls$formula=as.formula(formula)
+  ls$frm=formula
+  ls$alpha=alpha
+  ls$test=test
+  ls$nSimulation=nSimulation
+ 
+  # #logit regression for initial values
+  # lr <- glm(mdr$formula,mdr$data, family = quasibinomial("logit"), weights =mdr$weights)
+  # 
+  # # dummy model for technical reasons
+  # md= lm(mdr$frm, mdr$data)
+  # y=all.vars(as.formula(mdr$frm))[1]
+  # 
+  # 
+  # # logistic model for given parameters
+  # w=mdr$weights/mdr$n
+  # distance<-function(coef){
+  #   md$coefficients=coef
+  #   l=predict.lm(md,mdr$data)
+  #   (logistic(l)-mdr$data[[y]])*w
+  # }
+  # 
+  # # calculate minimum distance estimator
+  # 
+  # res=nls.lm(par=lr$coefficients, fn=distance)
+  # mdr$result.nls.lm=res
+  # 
+  # # calculate min distance
+  # mdr$min.distance=sqrt(deviance(res))
+  # mdr$coefficients=coef(res)
+  # 
+  # # calculate fitted
+  # md$coefficients=coef(res)
+  # l=predict.lm(md,mdr$data)
+  # mdr$fitted=logistic(l)
+  #  
+  # # easy access to other data 
+  # mdr$y=mdr$data[[y]]
+  # mdr$w=mdr$weights/mdr$n
+  # 
+  # # test results
+  # mdr$min.epsilon=NA
+  # 
+  # if (asymptotic==test) {
+  #   mdr$min.epsilon=asymptoticTest(mdr=mdr)
+  # }
+  # 
+  # if (asymptoticBootstrapVariance==test){
+  #   mdr$min.epsilon=asymptoticTestBootstrapVariance(mdr,nSimulation)
+  # }
+  # 
+  # if (empiricalBootstrap==test){
+  #   mdr$min.epsilon=empiricalBootstrapTest(mdr,nSimulation)
+  # }
+  # 
+  # if (tPercentileBootstrap==test){
+  #   mdr$min.epsilon=tPercentileBootstrapTest(mdr,nSimulation)
+  # }
+  # 
+  # return(ls)
+}
+
+# updateMinDistanceModel<-function(p,weights,mdr){
+#   df=mdr$data
+#   y=all.vars(as.formula(mdr$frm))[1]
+#   df[[y]]=p
+#   
+#   nlr=min_dst_logit(mdr$frm,data=df,weights=weights,test = mdr$test, alpha = mdr$alpha,
+#                     nSimulation = mdr$nSimulation)
+#   return(nlr)
+# }
--- distance.R
+++ distance.R
@@ -1,88 +1,21 @@
-distance<-function(x){
-  n=length(x)
-  r=0
+distance<-function(df,ab){
+  df=df[order(df$x),]
+  sf=0
+  sy=0
+  dst=0
+  n=nrow(df)
   
-  s<-function(x,y){
-    d=length(x)
-    s3=(1/3)^d
-    
-    r=s1(x,y)-s2(x)-s2(y)+s3
-    return(r)
-  }
-  s1<-function(x,y){
-    z=pmax(x,y)
-    z=1-z
-    return(prod(z))
-  }
-  s2<-function(x){
-    x=x*x
-    x=x/2
-    x=0.5-x
-    return(prod(x))
+  for(i in c(1:(n-1))){
+    sf=sf+df$f[i]
+    sy=sy+df$y[i]
+    dst=dst+((sf-sy)^2)*(df$x[i+1]-df$x[i])
   }
   
+  #special case i=n
+  sf=sf+df$f[n]
+  sy=sy+df$y[n]
+  dst=dst+((sf-sy)^2)*(ab[2]-df$x[n])
   
-  for(i in c(1:n)){
-    r=r+s(x[[i]],x[[i]])
-    
-    if (i+1>n) next
-    
-    for (j in c((i+1):n)){
-      r=r+2*s(x[[i]],x[[j]])
-    }
-  }
-  
-  r=r/n/n
-  return(r)
+  return(dst)
 }
 
-distanceMC<-function(x, nSimulation){
-  f<-function(x){
-    h=multivariate.ecdf(x,vx)
-    return((h-prod(x))^2)
-  }
-  
-  m=getMax(f,vx)
-  d=length(x[[1]])
-  res=integral.mc(f,d,m,nSimulation)
-  return(res)
-}
-
-distanceQuader<-function(m,a,b){
-  pa=prod(a)
-  pb=prod(b)
-  d=length(a)
-  
-  s1=pb-pa
-  
-  s2=(pb^2-pa^2)/(2^d)
-  
-  s3=(pb^3-pa^3)/(3^d)
-  
-  return(m*m*s1-2*m*s2+s3)
-}
-
-# distanceRegularHistogram<-function(h){
-#   n=length(h$rel.freq)
-#   sum=0
-#   
-#   for (i in c(1:n)){
-#     m=h$rel.freq[i]  
-#     a=h$mesh$S[1,,i]
-#     b=h$mesh$S[4,,i]
-#     sum=sum+distanceQuader(m,a,b)
-#   }
-#   
-#   return(sum)
-# }
-
-# getCDFRegularHistogram<-function(h,b){
-#   n=length(h$rel.freq)
-#   sum=0
-#   
-#   for (i in c(1:n)){
-#     a=h$mesh$S[4,,i]
-#     flag=as.logical(prod(a<=b))
-#     if (flag) {sum=sum+h$rel.freq(i)}
-#   }
-# }
