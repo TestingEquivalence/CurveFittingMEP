33cb65e037c13c227610311833ba6242d568c614
unknown <vladimir_ostrovski@web.de>
2023-06-07 20:15:49 +0200

    work on data

--- research/Hahn1.R
+++ research/Hahn1.R
@@ -1,10 +1,11 @@
 library(NISTnls)
-Try <- function(expr) if (!inherits(val <- try(expr), "try-error")) val
 
-Try(fm <- nls(y ~ (b1+b2*x+b3*x**2+b4*x**3) / (1+b5*x+b6*x**2+b7*x**3),
-               data = Hahn1, trace = TRUE,
+data=Hahn1
+frm="y ~ (b1+b2*x+b3*x**2+b4*x**3) / (1+b5*x+b6*x**2+b7*x**3)"
+
+fm <- nls(frn, data, 
                start = c(b1 = 10, b2 = -1, b3 = 0.05,
-                         b4 = -0.00001, b5 = -0.05, b6 = 0.001, b7 = -0.000001)))
+                         b4 = -0.00001, b5 = -0.05, b6 = 0.001, b7 = -0.000001))
 
 y=predict(fm)
 
--- research/concentration.R
+++ research/concentration.R
@@ -6,19 +6,16 @@ data=list()
 data$x=L.minor$conc
 data$y=L.minor$rate
 data=as.data.frame(data)
+data=data[order(data$x),]
 
-L.minor.m1 <- nls(rate ~ Vm*conc/(K+conc), data = L.minor, start = list(K=20, Vm=120))
-confint2(L.minor.m1)
-confint2(L.minor.m1, "K")
-
-plot(L.minor$conc,L.minor$rate)
-lines(L.minor$conc,predict(L.minor.m1), col="blue")
 
 frm="y ~ Vm*x/(K+x)"
-start= start = list(K=20, Vm=120)
+start= list(K=20, Vm=120)
 model.nls=nls(frm, data,start)
-coef(L.minor.m1)
 coef(model.nls)
+
+plot(data$x,data$y,ylim=c(0,120))
+lines(data$x,predict(model.nls), col="blue")
 
 # residuals
 data$f=predict(model.nls)
