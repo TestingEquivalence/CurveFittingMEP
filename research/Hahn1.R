745df1981eaeb95f334b8ee2208c1152a3a64360
unknown <vladimir_ostrovski@web.de>
2023-06-08 09:47:06 +0200

    work on data

--- research/Hahn1.R
+++ research/Hahn1.R
@@ -1,17 +1,18 @@
 library(NISTnls)
 
 data=Hahn1
+data=data[order(data$x),]
 frm="y ~ (b1+b2*x+b3*x**2+b4*x**3) / (1+b5*x+b6*x**2+b7*x**3)"
+start=c(b1 = 10, b2 = -1, b3 = 0.05,
+        b4 = -0.00001, b5 = -0.05, b6 = 0.001, b7 = -0.000001)
 
-fm <- nls(frn, data, 
-               start = c(b1 = 10, b2 = -1, b3 = 0.05,
-                         b4 = -0.00001, b5 = -0.05, b6 = 0.001, b7 = -0.000001))
+model.nls <- nls(frm,data, start)
 
-y=predict(fm)
+plot(data$x,data$y)
+lines(data$x,predict(model.nls), col="blue")
 
-df=data.frame(x=Hahn1$x, y)
-df=df[!duplicated(df$x),]
-df=df[order(df$x),]
-
-plot(y ~ x, data = Hahn1)
-lines(df$x,df$y, col="blue")
+# residuals
+data$f=predict(model.nls)
+data$res.nls=data$f-data$y
+plot(data$x,data$res.nls)
+hist(data$res.nls)
