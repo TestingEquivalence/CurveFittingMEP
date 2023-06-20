446476190423a8513218f57a028ad34bee58544f
unknown <vladimir_ostrovski@web.de>
2023-06-20 15:12:43 +0200

    work on data

--- research/Kirby2.R
+++ research/Kirby2.R
@@ -1,18 +1,30 @@
 library(NISTnls)
+source("distance.R")
+source("curveFittingMEP.R")
 
 data=Kirby2
 data=data[order(data$x),]
-frm="y ~ (b1 + b2*x + b3*x**2) / (1 + b4*x + b5*x**2)"
+frm=y ~ (b1 + b2*x + b3*x**2) / (1 + b4*x + b5*x**2)
 start=c(b1 = 2, b2 = -0.1, b3 = 0.003,
         b4 = -0.001, b5 = 0.00001)
 
-model.nls <- nls(frm,data, start)
+ab=c(9,372)
 
-plot(data$x,data$y)
-lines(data$x,predict(model.nls), col="blue")
 
-# residuals
-data$f=predict(model.nls)
-data$res.nls=data$f-data$y
-plot(data$x,data$res.nls)
-hist(data$res.nls)
+# model LSE
+m=curveFittingMEP(frm,data,none, ab, start, method = LSE)
+m$distance
+m$coef
+
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
+
+# model MDE
+m=curveFittingMEP(frm,data,none, ab, start, method = MDE)
+m$distance
+m$coef
+
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
--- research/Roszman1.R
+++ research/Roszman1.R
@@ -1,15 +1,28 @@
 library(NISTnls)
-Try <- function(expr) if (!inherits(val <- try(expr), "try-error")) val
-plot(y ~ x, data = Roszman1)
-Try(fm <- nls(y ~ b1 - b2*x - atan(b3/(x-b4))/pi, data = Roszman1,
-               start = c(b1 = 0.1, b2 = -0.00001, b3 = 1000, b4 = -100),
-               trace = TRUE))
+source("distance.R")
+source("curveFittingMEP.R")
 
-y=predict(fm)
+data=Roszman1
+data=data[order(data$x),]
+frm=y ~ b1 - b2*x - atan(b3/(x-b4))/pi
+start=c(b1 = 0.1, b2 = -0.00001, b3 = 1000, b4 = -100)
+ab=c(-4869,-464)
 
-df=data.frame(x=Roszman1$x, y)
-df=df[!duplicated(df$x),]
-df=df[order(df$x),]
 
-plot(y ~ x, data = Roszman1)
-lines(df$x,df$y, col="blue")
+# model LSE
+m=curveFittingMEP(frm,data,none, ab, start, method = LSE)
+m$distance
+m$coef
+
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
+
+# model MDE
+m=curveFittingMEP(frm,data,none, ab, start, method = MDE)
+m$distance
+m$coef
+
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
