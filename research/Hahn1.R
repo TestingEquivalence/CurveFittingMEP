3e9b1e2e11fe00c7d9abca0472cf85682a8eb032
unknown <vladimir_ostrovski@web.de>
2023-06-20 14:14:06 +0200

    work on data

--- research/Hahn1.R
+++ research/Hahn1.R
@@ -1,18 +1,30 @@
 library(NISTnls)
+source("distance.R")
+source("curveFittingMEP.R")
 
 data=Hahn1
 data=data[order(data$x),]
-frm="y ~ (b1+b2*x+b3*x**2+b4*x**3) / (1+b5*x+b6*x**2+b7*x**3)"
+frm=y ~ (b1+b2*x+b3*x**2+b4*x**3) / (1+b5*x+b6*x**2+b7*x**3)
 start=c(b1 = 10, b2 = -1, b3 = 0.05,
         b4 = -0.00001, b5 = -0.05, b6 = 0.001, b7 = -0.000001)
+ab=c(14,852)
 
-model.nls <- nls(frm,data, start)
 
-plot(data$x,data$y)
-lines(data$x,predict(model.nls), col="blue")
+# model LSE
+m=curveFittingMEP(frm,data,none, ab, start, method = LSE)
+m$distance
+m$coef
 
-# residuals
-data$f=predict(model.nls)
-data$res.nls=data$f-data$y
-plot(data$x,data$res.nls)
-hist(data$res.nls)
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
+
+
+# model MDE
+m=curveFittingMEP(frm,data,none, ab, start, method = MDE)
+m$distance
+m$coef
+
+plot(m$data$x, m$prediction-m$data$y)
+plot(m$data$x, m$prediction, col="blue",type="l")
+points(m$data$x,m$data$y)
--- research/concentration.R
+++ research/concentration.R
@@ -1,4 +1,4 @@
-library(nlstools)
+library(nlsMicrobio)
 source("distance.R")
 source("curveFittingMEP.R")
 
@@ -9,11 +9,12 @@ data$y=L.minor$rate
 data=as.data.frame(data)
 data=data[order(data$x),]
 
-# model LSE
-
 frm=y ~ Vm*x/(K+x)
 start= list(K=20, Vm=120)
 ab=c(0,205)
+
+# model LSE
+
 m=curveFittingMEP(frm,data,none, ab, start, method = LSE)
 m$distance
 m$coef
@@ -22,13 +23,11 @@ plot(m$data$x, m$prediction-m$data$y)
 plot(m$data$x, m$prediction, col="blue",type="l")
 points(m$data$x,m$data$y)
 
-
 # model MDE
 
-frm=y ~ Vm*x/(K+x)
-start= list(K=20, Vm=120)
-ab=c(0,205)
 m=curveFittingMEP(frm,data,none, ab, start, method = MDE)
+m$distance
+m$coef
 
 plot(m$data$x, m$prediction-m$data$y)
 plot(m$data$x, m$prediction, col="blue",type="l")
