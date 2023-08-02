bada0cb4df679aa3f518a2e62913f429b9d92670
unknown <vladimir_ostrovski@web.de>
2023-08-02 18:07:01 +0200

    tests fixed

--- tests/test11_extPoints.R
+++ tests/test11_extPoints.R
@@ -1,26 +1,20 @@
-library(nlsMicrobio)
+library(NISTnls)
 source("distance.R")
 source("curveFittingMEP.R")
 source("size.R")
 source("power.R")
 
-# conc is random
-data=list()
-data$x=L.minor$conc
-data$y=L.minor$rate
-data=as.data.frame(data)
+data=Roszman1
 data=data[order(data$x),]
-
-
-frm=y ~ Vm*x/(K+x)
-start= list(K=20, Vm=120)
-ab=c(0,205)
+frm=y ~ b1 - b2*x - atan(b3/(x-b4))/pi
+start=c(b1 = 0.1, b2 = -0.00001, b3 = 1000, b4 = -100)
+ab=c(-4869,-464)
 
 m=curveFittingMEP(frm,data,none, ab, start, method=LSE)
 
 f=randomPiecewiseLinear(m, xSamplerFix, errSamplerBootstrap, n=nrow(data)+2)
 
-x=seq(from=0, to=205, by=1)
+x=seq(from=ab[1], to=ab[2], by=1)
 y=sapply(x,f)
 y=cumsum(y)
 plot(x,y)
--- tests/test12_linearPoint.R
+++ tests/test12_linearPoint.R
@@ -1,20 +1,15 @@
-library(nlsMicrobio)
+library(NISTnls)
 source("distance.R")
 source("curveFittingMEP.R")
 source("size.R")
 source("power.R")
 
-# conc is random
-data=list()
-data$x=L.minor$conc
-data$y=L.minor$rate
-data=as.data.frame(data)
+data=Roszman1
 data=data[order(data$x),]
+frm=y ~ b1 - b2*x - atan(b3/(x-b4))/pi
+start=c(b1 = 0.1, b2 = -0.00001, b3 = 1000, b4 = -100)
+ab=c(-4869,-464)
 
-
-frm=y ~ Vm*x/(K+x)
-start= list(K=20, Vm=120)
-ab=c(0,205)
 
 m=curveFittingMEP(frm,data,none, ab, start, method=LSE)
 
@@ -24,8 +19,8 @@ f<-function(x){
   return(res)
 }
 
-x=seq(from=0, to=205, by=1)
+x=seq(from=ab[1], to=ab[2], by=10)
 
-w=0.1
+w=0.97
 y=linearPoint(m,f,w,x)
 plot(x,y)
