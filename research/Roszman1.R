2122e5332c9b8c27a7997f59bb9a55b836a295b9
unknown <vladimir_ostrovski@web.de>
2024-03-01 13:49:58 +0100

    works on simulation

--- research/Roszman1.R
+++ research/Roszman1.R
@@ -43,46 +43,48 @@ res=bootstrapCoef(m,1000)
 write.csv(res,"bst_coef_LM.csv")
  
 # power at the model LM LSE only
-m=curveFittingMEP(frm,data, asymptoticBV, ab, start, method = LM, nSimulation = 200, nSimPercentileTBootstrap = 200)
+m=curveFittingMEP(frm,data, asymptoticBV, ab, start, method = LM, nSimulation = 1000, nSimPercentileTBootstrap = 200)
 resPower=list()
 for (xKey in names(xSampler)){
   for (errKey in names(errSampler)){
-    resPower[[paste(xKey,errKey,sep="_")]]=powerAtModel(m,nSim=2, xSampler[[xKey]], errSampler[[errKey]])  
+    resPower[[paste(xKey,errKey,sep="_")]]=powerAtModel(m,nSim=1000, xSampler[[xKey]], errSampler[[errKey]])  
   }
 }
 
-write.csv(resPower,"resPower.csv")
+write.csv(resPower,"resSizeATBT1000.csv")
 
 # power at the boundary points based on sin(omega*x)
-omega=20
+vOmega=c(0.5, c(1:10))
+vXSampler=c()
+vErrSampler=c()
+vPower=c()
+vDst=c()
+vW=c()
 
-fsin<-function(x){
-  res=sin(2*pi*omega*(x-ab[1])/(ab[2]-ab[1]))
-  return(res)
+for (omega in vOmega){
+  fsin<-function(x){
+    res=sin(2*pi*omega*(x-ab[1])/(ab[2]-ab[1]))
+    return(res)
+  }
+  
+  eps=0.0010
+  dx=(ab[2]-ab[1])/1000
+  
+  m=curveFittingMEP(frm,data,asymptoticBV, ab, start, method = LM, nSimulation = 200)
+  w=linearBoundaryPoint(m,fsin,dx,eps,0.92,0.99999)
+  vW=c(vW,w)
+  
+  f<-function(x){
+    linearPoint(m,fsin,w,x)
+  } 
+  
+  dst=numericDistance(m,f,dx)
+  vDst=c(vDst,dst)
+  
+  pw=powerAtPoint(m,f,nSim=10, xSampler[[xKey]], errSampler[[errKey]],eps)
+  vPower=c(vPower,pw)
 }
 
-eps=5E-04
-dx=(ab[2]-ab[1])/1000
-
-m=curveFittingMEP(frm,data,asymptoticBV, ab, start, method = LM, nSimulation = 200)
-w=linearBoundaryPoint(m,fsin,dx,eps,0.92,0.99999)
-
-f<-function(x){
-  linearPoint(m,fsin,w,x)
-} 
-
-numericDistance(m,f,dx)
-
-powBstBst=powerAtPoint(m,f,nSim=1000, xSamplerBootstrap,errSamplerBootstrap,eps)
-powBstBst
-
-powBstNorm=powerAtPoint(m,f,nSim=1000, xSamplerBootstrap,errSamplerNormal,eps)
-powBstNorm
-
-powUniBst=powerAtPoint(m,f,nSim=1000, xSamplerUniform,errSamplerBootstrap,eps)
-powUniBst
-
-powUniNorm=powerAtPoint(m,f,nSim=1000, xSamplerUniform,errSamplerNormal,eps)
-powUniNorm
-
-
+res=data.frame(omega=vOmega, xSampler=vXSampler, errSampler=vErrSampler,
+               power=vPower, distance=vDst, w=vW)
+write.csv(resPower,"resPower.csv")
